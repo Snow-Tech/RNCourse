c3055aca777217bfac7a09190385f2fba955288c
Brian Vieira <1201913@isep.ipp.pt>
2024-10-02 11:11:55 +0100

    added remove funcionality in the list

--- App.js
+++ App.js
@@ -1,31 +1,42 @@
-import { useState } from 'react';
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
-import {  StyleSheet, View, FlatList } from 'react-native';
+import { useState } from "react";
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+import { StyleSheet, View, FlatList } from "react-native";
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
 
   function addGoalHandler(enteredGoalText) {
-     setCourseGoals((currentCourseGoals) => [
-      ...currentCourseGoals, 
-      {text: enteredGoalText, key: Math.random().toString}
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, key: Math.random().toString },
     ]);
+  }
+
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
     <View style={styles.appContainer}>
-    
-    <GoalInput onAddGoal={addGoalHandler} />
+      <GoalInput onAddGoal={addGoalHandler} />
 
-    <FlatList 
-    data={courseGoals} 
-    renderItem={(itemData) => {
-        return <GoalItem text={itemData.item.text} />;
-    }} 
-    style={styles.goalsContainer} />
-
-    </View> 
+      <FlatList
+        data={courseGoals}
+        style={styles.goalsContainer}
+        renderItem={(itemData) => {
+          return (
+            <GoalItem
+              text={itemData.item.text}
+              id={itemData.item.id}
+              onDeleteItem={deleteGoalHandler}
+            />
+          );
+        }}
+      />
+    </View>
   );
 }
 
@@ -33,10 +44,10 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     paddingTop: 100,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
   },
 
   goalsContainer: {
-    flex: 4
-  }
+    flex: 4,
+  },
 });
