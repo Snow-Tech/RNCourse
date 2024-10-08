b841c6a3487c42456f14517f7c72066d5735025a
Brian Vieira <1201913@isep.ipp.pt>
2024-10-08 22:28:39 +0100

    some improvements in the componenets and used linear gradient as background

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
+import { LinearGradient } from "expo-linear-gradient";
 import { StyleSheet } from "react-native";
 import StartGameVC from "./controllers/StartGameVC";
 
 export default function App() {
-  return <StartGameVC />;
+  return (
+    <LinearGradient colors={["#ddb52f", "#3b021f"]} style={styles.rootView}>
+      <StartGameVC />
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootView: {
+    flex: 1,
+  },
+});
--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,46 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function PrimaryButton({ children }) {
+  function didTapButton() {
+    console.log("oi");
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={didTapButton}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
